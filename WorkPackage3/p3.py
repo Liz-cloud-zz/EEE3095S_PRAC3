7bbbc09cfe5905a0755d3c0d59c45c011b95673f
Liz-cloud <mfnlin003@myuct.ac.za>
2021-09-05 04:21:43 +0100

    TEST1

--- WorkPackage3/p3.py
+++ WorkPackage3/p3.py
@@ -6,7 +6,7 @@ import os
 import signal
 import sys
 import time
-
+import math
 # some global variables that need to change as we run the program
 end_of_game = None  # set if the user wins or ends the game
 
@@ -15,9 +15,15 @@ LED_value = [11, 13, 15]
 LED_accuracy = 32
 btn_submit = 16
 btn_increase = 18
-buzzer = None
+buzzer = 33
 eeprom = ES2EEPROMUtils.ES2EEPROM()
-pi_pwn=0
+global pi_pwnL,pi_pwnB
+pi_pwnL=None
+pi_pwnB=None
+global curr_guess
+curr_guess=0
+global username
+username=None
 
 
 # Print the game banner
@@ -30,7 +36,7 @@ def welcome():
     print("| |\  | |_| | | | | | | |_) |  __/ |     ____) | | | | |_| | | | | | |  __/")
     print("|_| \_|\__,_|_| |_| |_|_.__/ \___|_|    |_____/|_| |_|\__,_|_| |_| |_|\___|")
     print("")
-    print("Guess the number and immortalise your name in the High Score Hall of Fame!")
+    print("curr_guess the number and immortalise your name in the High Score Hall of Fame!")
 
 
 # Print the game menu
@@ -46,8 +52,8 @@ def menu():
     elif option == "P":
         os.system('clear')
         print("Starting a new round!")
-        print("Use the buttons on the Pi to make and submit your guess!")
-        print("Press and hold the guess button to cancel your game")
+        print("Use the buttons on the Pi to make and submit your curr_guess!")
+        print("Press and hold the curr_guess button to cancel your game")
         value = generate_number()
         while not end_of_game:
             pass
@@ -65,41 +71,51 @@ def display_scores(count, raw_data):
     pass
 
 #use the signal module to be able to handle and make the program pause indefinitely.
-def signal_handler(sig, frame):
-    GPIO.cleanup()
-    sys.exit(0)
+# def signal_handler(sig, frame):
+#     GPIO.cleanup()
+#     sys.exit(0)
 
 # Setup Pins
 def setup():
+    GPIO.setwarnings(False)
     # Setup board mode
     GPIO.setmode(GPIO.BOARD)
+    print("setmode is Board")
 
-    # Setup regular GPIO:
-    # Set LEDS for outputs 
-    for i in range (len(LED_value)):
-        GPIO.setup(LED_value[i], GPIO.OUTPUT) # set GPIO pins as input  
-    GPIO.set(LED_accuracy,GPIO.OUTPUT)
-    # Set buttons for input
-    GPIO.setup(btn_increase,GPIO.IN,pull_up=GPIO.PUD_UP)
-    GPIO.setup(btn_submit,GPIO.IN,pull_up=GPIO.PUD_UP)
+    try:
+        # Setup regular GPIO:
+        # Set LEDS for outputs 
+        for i in range (len(LED_value)):
+            GPIO.setup(LED_value[i], GPIO.OUT) # set GPIO pins as input  
+        GPIO.setup(LED_accuracy,GPIO.OUT)
+        # Set buttons for input
+        GPIO.setup(btn_increase,GPIO.IN,pull_up_down=GPIO.PUD_UP)
+        GPIO.setup(btn_submit,GPIO.IN,pull_up_down=GPIO.PUD_UP)
+        #Set buzzer for output
+        GPIO.setup(buzzer,GPIO.OUT)
 
-    # Setup PWM channels
-    pi_pwn=GPIO.PWM(LED_accuracy,1000)
-    # It is used to start PWM generation of specified Duty Cycle.
-    pi_pwn.start(0)
-    # Setup debouncing and callbacks
-    GPIO.add_event_detect(btn_increase, GPIO.FALLING,callback=btn_increase_pressed, bouncetime=100)
-    signal.signal(signal.SIGINT, signal_handler)
-    signal.pause()
+        # Setup PWM channels
+        pi_pwnL=GPIO.PWM(LED_accuracy,1000)# for LED
+        pi_pwnL=GPIO.PWM(buzzer,1000)# for Buzzer
+        # It is used to start PWM generation of specified Duty Cycle.
+        pi_pwnL.start(0)
+        # Setup debouncing and callbacks
+        GPIO.add_event_detect(btn_increase, GPIO.RISING,callback=btn_increase_pressed, bouncetime=100)
+        GPIO.add_event_detect(btn_submit, GPIO.RISING,callback=btn_curr_guess_pressed, bouncetime=100)
 
-    GPIO.add_event_detect(btn_submit, GPIO.FALLING,callback=btn_guess_pressed, bouncetime=100)
-    signal.signal(signal.SIGINT, signal_handler)
-    signal.pause()
-    
+    except KeyboardInterrupt: # If CTRL+C is pressed, exit cleanly:
+        print("Keyboard interrupt")
+
+    except:
+        print("some error") 
+
+    finally:
+        print("clean up") 
+        GPIO.cleanup() # cleanup all GPIO 
     pass
 
 # convert to ASCII val
-def convertASC(val=None):
+def converttoASC(val=None):
     return ''.join([chr(int(x, 2)) for x in val])
 
 # Load high scores
@@ -122,149 +138,106 @@ def save_scores():
     # fetch scores
     score_count, scores=fetch_scores()
     # include new score
-
+    scores.append([username,curr_guess])
     # sort
     scores.sort(key=lambda x: x[1])
     # update total amount of scores
-
+    score_count+=1
     # write new scores
     eeprom.write_block(0,scores)
     pass
 
 
-# Generate guess number
+# Generate curr_guess number
 def generate_number():
     return random.randint(0, pow(2, 3)-1)
-# LED blink method
-def blinker(should_blink,LED_GPIO)
-     while True:
-        if should_blink:
-            GPIO.output(LED_GPIO, GPIO.HIGH) 
-        time.sleep(0.5)
-        if should_blink:
-            GPIO.output(LED_GPIO, GPIO.LOW)  
-        time.sleep(0.5)
-    
+
 # Increase button pressed
 def btn_increase_pressed(channel):
     # Increase the value shown on the LEDs
-    # You can choose to have a global variable store the user's current guess, 
-    global guess=0
-    global should_blink=False
-    
-    # or just pull the value off the LEDs when a user makes a guess
+    # You can choose to have a global variable store the user's current curr_guess, 
+    # or just pull the value off the LEDs when a user makes a curr_guess
     if not GPIO.input(btn_increase):
-        guess+=1# increment guess number
-        if (guess==0): # LED1=OFF, LED2=OFF, LED3=OFF
-            GPIO.output(LED_value[0],GPIO.LOW)
-
-        elif(guess==1):# LED1=BLINK, LED2=OFF, LED3=OFF
-            should_blink=not should_blink
-            blinker(should_blink,LED_value[0])
-
-        elif(guess==2):# LED1=ON, LED2=OFF, LED3=OFF
-            GPIO.output(LED_value[0],GPIO.HIGH)
-
-        elif (guess==3):# LED1=BLINK, LED2=BLINK, LED3=OFF
-            should_blink=not should_blink
-            blinker(should_blink,LED_value[0])
-            
-            should_blink=not should_blink
-            blinker(should_blink,LED_value[1])
-
-        elif(guess==4):# LED1=ON, LED2=BLINK, LED3=OFF
-            GPIO.output(LED_value[0],GPIO.HIGH)
-            should_blink=not should_blink
-            blinker(should_blink,LED_value[1])
-
-        elif(guess==5):# LED1=ON, LED2=ON, LED3=OFF
-            GPIO.output(LED_value[0],GPIO.HIGH)
-            GPIO.output(LED_value[1],GPIO.HIGH)
-
-        elif (guess==6):# LED1=BLINK, LED2=BLINK, LED3=BLINK
-            should_blink=not should_blink
-            blinker(should_blink,LED_value[0])
-            
-            should_blink=not should_blink
-            blinker(should_blink,LED_value[1])
-            
-            should_blink=not should_blink
-            blinker(should_blink,LED_value[2])
-
-        elif(guess==7):# LED1=HIGH, LED2=ON, LED3=BLINK
-            GPIO.output(LED_value[0],GPIO.HIGH)
-            GPIO.output(LED_value[1],GPIO.HIGH)
-
-            should_blink=not should_blink
-            blinker(should_blink,LED_value[2])
-
-        elif(guess==8):# LED1=ON, LED2=ON, LED3=ON
-            GPIO.output(LED_value[0],GPIO.HIGH)
-            GPIO.output(LED_value[1],GPIO.HIGH)
-            GPIO.output(LED_value[2],GPIO.HIGH)
+        print("button increase pressed!")
+        curr_guess+=1# increment curr_guess number
+        if curr_guess==1:# LED1=ON, LED2=OFF, LED3=OFF
+            LED_value[0]=GPIO.HIGH
+            LED_value[1]=GPIO.LOW
+            LED_value[2]=GPIO.LOW
+        elif curr_guess==2:# LED1=OFF, LED2=ON, LED3=OFF
+            LED_value[0]=GPIO.LOW
+            LED_value[1]=GPIO.HIGH
+            LED_value[2]=GPIO.LOW
+        elif curr_guess==3:# LED1=ON, LED2=ON, LED3=OFF
+            LED_value[0]=GPIO.HIGH
+            LED_value[1]=GPIO.HIGH
+            LED_value[2]=GPIO.LOW
+        elif curr_guess==4:# LED1=OFF, LED2=OFF, LED3=ON
+            LED_value[0]=GPIO.LOW
+            LED_value[1]=GPIO.LOW
+            LED_value[2]=GPIO.HIGH
+        elif curr_guess==5:# LED1=ON, LED2=OFF, LED3=ON
+            LED_value[0]=GPIO.HIGH
+            LED_value[1]=GPIO.LOW
+            LED_value[2]=GPIO.HIGH
+        elif curr_guess==6:# LED1=OFF, LED2=ON, LED3=ON
+            LED_value[0]=GPIO.LOW
+            LED_value[1]=GPIO.HIGH
+            LED_value[2]=GPIO.HIGH
+        elif curr_guess==7:# LED1=ON, LED2=ON, LED3=ON
+            LED_value[0]=GPIO.HIGH
+            LED_value[1]=GPIO.HIGH
+            LED_value[2]=GPIO.HIGH
     else:
         print("button increase released!")
-        GPIO.output(LED_value[i],GPIO.LOW)
-     pass
+        GPIO.OUT(LED_value[i],GPIO.LOW)
+    pass
 
 
-# Guess button
-def btn_guess_pressed(channel):
+# curr_guess button
+def btn_curr_guess_pressed(channel):
     # If they've pressed and held the button, clear up the GPIO and take them back to the menu screen
-      if not GPIO.input(btn_submit):
-         print("Button submit pressed!")
-         GPIO.cleanup()
-         menu()
-         # Compare the actual value with the user value displayed on the LEDs
-         # Change the PWM LED
-         # if it's close enough, adjust the buzzer
-         if (value==guess):  # if it's an exact guess:
-             for i in range (len(LED_value)): # - Disable LEDs and Buzzer
-                 GPIO.output(LED_value[i], LOW)
-             # - tell the user and prompt them for a name
-             username=input()
-             print("Enter your name: "+username)
-             # - fetch all the scores
-             score_count, scores=fetch_scores()
-             # - add the new score
-             # - sort the scores
-             scores.sort(key=lambda x: x[1])
-             # - Store the scores back to the EEPROM, 
-             # being sure to update the score count
-             eeprom.write_block(0,scores)
-             
+    if not GPIO.input(btn_submit):
+        print("Button submit pressed!")
+        #GPIO.cleanup()
+        menu()
+        # Compare the actual value with the user value displayed on the LEDs
+        # Change the PWM LED
+        accuracy_leds()
+        # if it's close enough, adjust the buzzer
+        if (value==curr_guess):  # if it's an exact curr_guess:
+            # - Disable LEDs and Buzzer
+            for i in range (len(LED_value)): 
+                GPIO.OUT(LED_value[i], LOW)
+            pi_pwnL.stop()
+            pi_pwnB.stop()
+            # - tell the user and prompt them for a name
+            username=input()
+            print("Enter your name: "+username)
+            # - fetch all the scores
+            # - add the new score
+            # - sort the scores
+            # - Store the scores back to the EEPROM, 
+            # being sure to update the score count
+            save_scores()
+          
     pass
 
 
 # LED Brightness
 def accuracy_leds():
-    # Set the brightness of the LED based on how close the guess is to the answer
+    # Set the brightness of the LED based on how close the curr_guess is to the answer
     # - The % brightness should be directly proportional to the % "closeness"
-    # - For example if the answer is 6 and a user guesses 4, the brightness should be at 4/6*100 = 66%
-    # - If they guessed 7, the brightness would be at ((8-7)/(8-6)*100 = 50%
-        
-    # ChangeDutyCycle(Duty Cycle)
-
-    # This function is used to change the Duty Cycle of signal. We have to provide Duty Cycle in the range of 0-100.
-    # ChangeFrequency(frequency)
-
-    # This function is used to change the frequency (in Hz) of PWM. 
-    # This function we have not used in above program. But, we can use it for changing the frequency.
+    # - For example if the answer is 6 and a user curr_guesses 4, the brightness should be at 4/6*100 = 66%
+    # - If they curr_guessed 7, the brightness would be at ((8-7)/(8-6)*100 = 50%
    try:
-       while True:
-            for duty in range(0,101,1):
-                pi_pwn.ChangeDutyCycle(duty)
-                sleep(0.01)
-        sleep(0.5)
-
-        for duty in range(100,-1,-1):
-            pi_pwn.ChangeDutyCycle(duty)
-            sleep(0.01)
-        sleep(0.5)
-    except KeyboardInterrupt:
-    pass
-    pi_pwn.stop()
-    GPIO.cleanup()
+       duty=((8-curr_guess)/(8-value))*100
+       pi_pwnL.ChangeDutyCycle(duty)
+       sleep(0.01)
+   except KeyboardInterrupt:
+       print("interrupt occured!")
+   pass
+    
 
 # Sound Buzzer
 def trigger_buzzer():
@@ -274,7 +247,27 @@ def trigger_buzzer():
     # If the user is off by an absolute value of 3, the buzzer should sound once every second
     # If the user is off by an absolute value of 2, the buzzer should sound twice every second
     # If the user is off by an absolute value of 1, the buzzer should sound 4 times a second
-
+    diff=abs(value-curr_guess)
+    try:
+        if(diff==3):
+            pi_pwnB.ChangeDutyCycle(50)# 1 sec
+            sleep(0.1)
+        elif(diff==2):
+            pi_pwnB.ChangeDutyCycle(50)#1 sec
+            sleep(0.1)
+            pi_pwnB.ChangeDutyCycle(50)# 2sec
+            sleep(0.1)
+        elif(diff==1):
+            pi_pwnB.ChangeDutyCycle(50)# 1 sec
+            sleep(0.1)
+            pi_pwnB.ChangeDutyCycle(50)# 2 sec
+            sleep(0.1)
+            pi_pwnB.ChangeDutyCycle(50)# 3 sec
+            sleep(0.1)
+            pi_pwnB.ChangeDutyCycle(50)# 4 sec
+            sleep(0.1)
+    except KeyboardInterrupt:
+        print("interrupt occured!")
     pass
 
 
@@ -282,10 +275,14 @@ def trigger_buzzer():
 if __name__ == "__main__":
     try:
         # Call setup function
+        print("main method evocked")
         setup()
+        print("set up done!")
         welcome()
+        print("wlecome message displayed")
         while True:
             menu()
+            print("menu displayed")
             pass
     except Exception as e:
         print(e)
--- WorkPackage3/setup.py
+++ WorkPackage3/setup.py
@@ -1,15 +1,86 @@
 import RPi.GPIO as GPIO
+import random
+import ES2EEPROMUtils
+import os
+import signal
+import sys
+import time
 
-def setUp():
+def main():
+    GPIO.setwarnings(False)
+    print("setup!")
     GPIO.setmode(GPIO.BCM)
-    GPIO.setup(23, GPIO.IN)
-    GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_UP)
-    print(GPIO.input(23))
-    print(not GPIO.input(23))
-    GPIO.setup([12,13,17,27,22], GPIO.OUT)
+    try:
+        # GPIO.setup(23, GPIO.IN)
+        GPIO.setup(23, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+        print(GPIO.input(23))
+        print(not GPIO.input(23))
+        GPIO.setup([12,13,17,27,22], GPIO.OUT)
+        GPIO.add_event_detect(23, GPIO.RISING,callback=btn_increase_pressed, bouncetime=100)
+    except KeyboardInterrupt: # If CTRL+C is pressed, exit cleanly:
+        print("Keyboard interrupt")
+
+    except:
+        print("some error") 
+
+    finally:
+        print("clean up") 
+        GPIO.cleanup() # cleanup all GPIO 
+    
+def btn_increase_pressed():
     while not GPIO.input(23) == GPIO.LOW:
         GPIO.output([12, 13,17,27,22], GPIO.HIGH)
+    print("button was pressed! ")
+if __name__ == "__main__":
+    main()
 
-    print("button was pressed")
+# some global variables that need to change as we run the program
+# end_of_game = None  # set if the user wins or ends the game
 
-    GPIO.cleanup()
+# # DEFINE THE PINS USED HERE
+# LED_value = [11, 13, 15]
+# LED_accuracy = 32
+# btn_submit = 16
+# btn_increase = 18
+# buzzer = None
+# eeprom = ES2EEPROMUtils.ES2EEPROM()
+# global pi_pwn
+# global guess
+# global should_blink
+# guess=0
+# should_blink=False
+# global username
+
+# #use the signal module to be able to handle and make the program pause indefinitely.
+# def signal_handler(sig, frame):
+#     GPIO.cleanup()
+#     sys.exit(0)
+
+# # Setup Pins
+# def setup():
+#     # Setup board mode
+#     GPIO.setmode(GPIO.BOARD)
+
+#     # Setup regular GPIO:
+#     # Set LEDS for outputs 
+#     for i in range (len(LED_value)):
+#         GPIO.setup(LED_value[i], GPIO.OUTPUT) # set GPIO pins as input  
+#     GPIO.set(LED_accuracy,GPIO.OUTPUT)
+#     # Set buttons for input
+#     GPIO.setup(btn_increase,GPIO.IN,pull_up=GPIO.PUD_UP)
+#     GPIO.setup(btn_submit,GPIO.IN,pull_up=GPIO.PUD_UP)
+
+#     # Setup PWM channels
+#     pi_pwn=GPIO.PWM(LED_accuracy,1000)
+#     # It is used to start PWM generation of specified Duty Cycle.
+#     pi_pwn.start(0)
+#     # Setup debouncing and callbacks
+#     GPIO.add_event_detect(btn_increase, GPIO.FALLING,callback=btn_increase_pressed, bouncetime=100)
+#     signal.signal(signal.SIGINT, signal_handler)
+#     signal.pause()
+
+#     GPIO.add_event_detect(btn_submit, GPIO.FALLING,callback=btn_guess_pressed, bouncetime=100)
+#     signal.signal(signal.SIGINT, signal_handler)
+#     signal.pause()
+    
+#     pass
